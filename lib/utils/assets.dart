42cf0a83df929da4e38c6899a7ab918364206549
Sylvester-git <sylvesterpauldaviditzspd123@gmail.com>
2025-08-19 01:08:53 +0100

    Add new SVG icons for various UI elements
    - Added QrCode.svg for QR code representation.
    - Added arrow-left-down-line.svg for left-down arrow icon.
    - Added arrow-right-up-line.svg for right-up arrow icon.
    - Added error-warning-fill.svg for error warning icon.
    - Added eye-line.svg for eye icon representation.
    - Added home.svg for home icon.
    - Added lock-2-line.svg for lock icon.
    - Added mail-line.svg for mail icon.
    - Added profile.svg for user profile icon.
    - Added question-line.svg for question mark icon.
    - Added select-box-circle-fill.svg for filled circle select box.
    - Added star.svg for star icon representation.

--- lib/utils/assets.dart
+++ lib/utils/assets.dart
@@ -1,5 +1,23 @@
-class SvgAssets {}
+class SvgAssets {
+  static const pointsEarned = "assets/svg/arrow-left-down-line.svg";
+  static const pointsClaimed = "assets/svg/arrow-right-up-line.svg";
+  static const reward = "assets/svg/star.svg";
+  static const avatar = "assets/svg/Avatar.svg";
+  static const error = "assets/svg/error-warning-fill.svg";
+  static const hidepassword = "assets/svg/eye-line.svg";
+  static const home = "assets/svg/home.svg";
+  static const color = "assets/svg/lock-2-line.svg";
+  static const email = "assets/svg/mail-line.svg";
+  static const brandlogo1 = "assets/svg/MajorBrandLogos.svg";
+  static const profile = "assets/svg/profile.svg";
+  static const qrcode = "assets/svg/QrCode.svg";
+  static const help = "assets/svg/question-line.svg";
+  static const success = "assets/svg/select-box-circle-fill.svg";
+}
 
-class ImageAssets {}
-
-class LottieAssets {}
+class ImageAssets {
+  static const coldstoneSmall = "assets/images/cold-stone-sml.png";
+  static const coldstoneBig = "assets/images/cold-stone-big.png";
+  static const orikiSmall = "assets/images/oriki-sml.png";
+  static const orikiBig = "assets/images/oriki-big.png";
+}
